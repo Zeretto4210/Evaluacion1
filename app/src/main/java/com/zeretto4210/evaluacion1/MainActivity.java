aa52123f281eddc34dd63c02d92e19b1f42dcd96
Zeretto4210 <m.aguayo9@alumnos.santotomas.cl>
2020-09-11 20:51:26 -0300

    Agregada la verificación del valor mínimo $190 (no se agrega al registro si no supera ese monto) Agregado el folio inicial 100 (Interno) Arreglado el valor mínimo (0 no tiene sentido)

--- app/src/main/java/com/zeretto4210/evaluacion1/MainActivity.java
+++ app/src/main/java/com/zeretto4210/evaluacion1/MainActivity.java
@@ -10,42 +10,46 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private int count;
+    private int starter;
     private int[] highvalue;
     private int[] minvalue;
     private int totalvalue;
     private int avgvalue;
     private TextView report;
     private TextView number;
+    private String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        starter = 100; // folio de inicio (?)
         count = 0;
         highvalue = new int[] {0,0}; //[0] = folio, [1] = valor
-        minvalue = new int[] {0,0};  //[0] = folio, [1] = valor
+        minvalue = new int[] {0,2147483640};  //[0] = folio, [1] = valor
         totalvalue = 0;
         avgvalue = 0;
         report = (TextView) findViewById(R.id.l_report);
         number  = (TextView) findViewById(R.id.i_value);
+        text = "";
     }
     public void press(View v){
         int value = Integer.parseInt(""+number.getText());
         number.setText("");
+        if(value >=190){
+            count++;
+            if(value>highvalue[1]){
+                highvalue[0] = count+starter;
+                highvalue[1] = value;
+            }
+            if(value<minvalue[1]){
+                minvalue[0] = count+starter;
+                minvalue[1] = value;
+            }
+            totalvalue += value;
+            avgvalue = totalvalue/count;
 
-        count++;
-        if(value>highvalue[1]){
-            highvalue[0] = count;
-            highvalue[1] = value;
+            text = " Boletas Ingresadas: "+count+"\n Boleta Mayor: Folio "+highvalue[0]+" - Valor $"+highvalue[1]+"\n Boleta Menor: Folio "+minvalue[0]+" - Valor $"+minvalue[1]+"\n Ingresos Totales: $"+totalvalue+"\n Promedio: $"+avgvalue+"\n Primer Folio: "+starter+"\n Ultimo Folio: "+(count+starter)+"";
         }
-        if(value<minvalue[1]){
-            minvalue[0] = count;
-            minvalue[1] = value;
-        }
-        totalvalue += value;
-        avgvalue = totalvalue/count;
-
-        String text = " Boletas Ingresadas: "+count+"\n Boleta Mayor: Folio "+highvalue[0]+" - Valor $"+highvalue[1]+"\n Boleta Menor: Folio"+minvalue[0]+" - Valor $"+minvalue[1]+"\n Ingresos Totales: $"+totalvalue+"\n Promedio: $"+avgvalue+"\n Primer Folio: 1\n Ultimo Folio: "+count+"";
-
         report.setText(text);
     }
 }
